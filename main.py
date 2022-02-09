7401a0dbaee503914842659c5778758ce80a5637
Вячеслав Федоренко <slavafedora@yandex.ru>
2022-02-09 09:00:11 +0300

    add keyboard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient;
 import telebot;
+from telebot import types
 import setting;
 import func;
 
@@ -31,6 +32,14 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Ты успешно зарегистрирован!")
         return
 
+    elif (message.text == "/тест"):
+        keyboard = types.InlineKeyboardMarkup();
+        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes');
+        key_no = types.InlineKeyboardButton(text='Нет', callback_data='no', url='https://hrw.test.urentbike.ru');
+        keyboard.add(key_yes).add(key_no);
+        question = 'Вот клавиатура:';
+        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
     elif (message.text == "/start"):
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?\nВведи: /help, что-бы получить список команд!")
 
@@ -57,5 +66,12 @@ def get_text_messages(message):
 
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help, что-бы получить список моих команд.")
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, 'Ты ответил да?');
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, 'Ты ответил нет?');
 
 bot.polling(none_stop=True, interval=0)
