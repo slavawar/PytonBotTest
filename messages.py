cbc12b64f7bb537ff22229ac9133027c98074ede
Вячеслав Федоренко <slavafedora@yandex.ru>
2022-02-10 18:15:35 +0300

    up to case

--- messages.py
+++ messages.py
@@ -12,49 +12,49 @@ def get_text_messages(message, collection, bot):
         bot.send_message(message.from_user.id, 'Ты не зарегистрирован!\nИспользуй команду: /reg')
         return
 
-    if (message.text == "/reg"):
-        if (_user != None):
-            bot.send_message(message.from_user.id, "Ты уже зарегистрирован!")
+    match message.text:
+        case "/reg":
+            if (_user != None):
+                bot.send_message(message.from_user.id, "Ты уже зарегистрирован!")
+                return
+            count_users = collection.count_documents({})
+            reg = { "id": count_users + 1, "uid": message.from_user.id, "name": message.from_user.first_name }
+            func.insert_document(collection, reg)
+            bot.send_message(message.from_user.id, "Ты успешно зарегистрирован!")
             return
-        count_users = collection.count_documents({})
-        reg = { "id": count_users + 1, "uid": message.from_user.id, "name": message.from_user.first_name }
-        func.insert_document(collection, reg)
-        bot.send_message(message.from_user.id, "Ты успешно зарегистрирован!")
-        return
 
-    elif (message.text == "/тест"):
-        keyboard = types.InlineKeyboardMarkup();
-        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes');
-        base_url = "https://hrw.test.urentbike.ru"
-        key_no = types.InlineKeyboardButton(text='Нет', callback_data='no', url=base_url);
-        keyboard.add(key_yes).add(key_no);
-        question = 'Вот клавиатура:';
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+        case "/тест":
+            keyboard = types.InlineKeyboardMarkup();
+            key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes');
+            base_url = "https://hrw.test.urentbike.ru"
+            key_no = types.InlineKeyboardButton(text='Нет', callback_data='no', url=base_url);
+            keyboard.add(key_yes).add(key_no);
+            question = 'Вот клавиатура:';
+            bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+        
+        case "/start":
+            bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?\nВведи: /help, что-бы получить список команд!")
 
-    elif (message.text == "/start"):
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?\nВведи: /help, что-бы получить список команд!")
+        case "/help":
+            bot.send_message(message.from_user.id, 
+            "Вот список моих команд:" + 
+            "\n/help - вывести список команд" + 
+            "\n/list - Список пользователей" + 
+            "\n/profile - Профиль" +
+            "\n/reg - Регистрация")
 
-    elif (message.text == "/help"):
-        bot.send_message(message.from_user.id, 
-        "Вот список моих команд:" + 
-        "\n/help - вывести список команд" + 
-        "\n/list - Список пользователей" + 
-        "\n/profile - Профиль" +
-        "\n/reg - Регистрация")
-
-    elif (message.text == "/profile"):        
-        bot.send_message(message.from_user.id, 
+        case "/profile":
+            bot.send_message(message.from_user.id, 
             '\nТвой профиль,' + 
             f'\nИмя: {_user["name"]}' +
-            f'\nID Профиля: {str(_user["id"])}'
-        )
+            f'\nID Профиля: {str(_user["id"])}')
 
-    elif message.text == "/list":
-        users = func.find_document(collection, {}, True)
-        list = ''
-        for user in users:
-            list = f'{list}[{user["id"]}] {user["name"]}\n'
-        bot.send_message(message.from_user.id, f'Список пользователей в базе:\n{list}')
+        case "/list":
+            users = func.find_document(collection, {}, True)
+            list = ''
+            for user in users:
+                list = f'{list}[{user["id"]}] {user["name"]}\n'
+            bot.send_message(message.from_user.id, f'Список пользователей в базе:\n{list}')
 
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help, что-бы получить список моих команд.")
+        case _:
+            bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help, что-бы получить список моих команд.")
