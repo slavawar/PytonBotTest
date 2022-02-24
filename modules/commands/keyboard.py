467fb2354fa7d51be6e8fdfd999cb13319f98959
Вячеслав Федоренко <slavafedora@yandex.ru>
2022-02-24 11:13:36 +0300

    .vscode in gitignore

---
+++ modules/commands/keyboard.py
@@ -0,0 +1,12 @@
+from telebot import types
+
+tag = ['keyboard']
+button = []
+
+def funca(message, collection, bot, _user):
+    keyboard = types.InlineKeyboardMarkup();
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='USER_ACCEPT');
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='USER_DECLINE');
+    keyboard.add(key_yes).add(key_no);
+    question = 'Вот кнопки';
+    bot.send_message(message.from_user.id, text = question, reply_markup = keyboard)
--- modules/commands/test.py
+++ modules/commands/test.py
@@ -4,9 +4,4 @@ tag = ['test', 'тест']
 button = []
 
 def funca(message, collection, bot, _user):
-    keyboard = types.InlineKeyboardMarkup();
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data='USER_ACCEPT');
-    key_no = types.InlineKeyboardButton(text='Нет', callback_data='USER_DECLINE');
-    keyboard.add(key_yes).add(key_no);
-    question = 'Вот кнопки';
-    bot.send_message(message.from_user.id, text = question, reply_markup = keyboard)
+    f = lambda x: x + x
